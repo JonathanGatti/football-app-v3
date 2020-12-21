3e6f3b9acdb011664c3764570fa7f803ab5fbda0
jonathan gatti <jonathangatti2@gmail.com>
2020-12-21 17:55:04 +0000

    router set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import CreateTeam from './components/CreateTeam';
+import ShowTeam from './components/ShowTeam';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/" render={() => <Home />} />
+        <Route exact path="/team/create" render={() => <CreateTeam />} />
+        <Route exact path="/team/show" render={() => <ShowTeam />} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/ShowTeam.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ShowTeam() {
+  return <div>ShowTeam</div>;
+}
+
+export default ShowTeam;
