421bf1e1a11b4dc1c5c1ccf010c53709c9c0a5d3
jonathan gatti <jonathangatti2@gmail.com>
2020-12-23 13:19:02 +0000

    search player input

--- src/pages/CreateTeam.tsx
+++ src/pages/CreateTeam.tsx
@@ -1,15 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getPlayer } from '../api/externalApi';
 
 function CreateTeam() {
-  useEffect(() => {
-    async function getPlayerInEffect() {
-      const res = await getPlayer('cristiano ronaldo');
-      console.log(res);
-    }
-    getPlayerInEffect();
-  }, []);
-  return <div>CreateTeam</div>;
+  const [name, setName] = useState('');
+  // useEffect(() => {
+  //   async function getPlayerInEffect() {
+  //     const res = await getPlayer('');
+  //     console.log(res);
+  //   }
+  //   getPlayerInEffect();
+  // }, []);
+
+  const handleSubmit = (e: any) => {
+    e.stopPropagation();
+    console.log(e.target.value);
+  };
+  return (
+    <div>
+      <h1>Create Team</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" />
+      </form>
+    </div>
+  );
 }
 
 export default CreateTeam;
