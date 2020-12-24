e9e5f736c5628c87766d6e3c2aca70c27f2e9bb2
jonathan gatti <jonathangatti2@gmail.com>
2020-12-24 11:05:25 +0000

    teamModules

--- src/pages/CreateTeam.tsx
+++ src/pages/CreateTeam.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchPlayerForm from '../components/SearchPlayerForm';
+import { classic, defensive, offensive } from '../utils/teamModules';
+
 function CreateTeam() {
+  const [module, setModule] = useState(offensive);
+
   return (
     <div>
       <h1>Create Team</h1>
-      <SearchPlayerForm />
+      <div>
+        <div className="ui centered grid">
+          {module.map((size) => (
+            <div className={`${size} wide column`}>
+              <SearchPlayerForm />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/utils/teamModules.ts
@@ -0,0 +1,15 @@
+export const classic = [
+  'sixteen',
+  'four',
+  'four',
+  'four',
+  'four',
+  'four',
+  'four',
+  'four',
+  'four',
+  'six',
+  'six',
+];
+export const defensive = ['sixteen', 'three', 'three', 'three', 'three', 'three', 'five', 'five', 'five', 'six', 'six'];
+export const offensive = ['sixteen', 'four', 'four', 'four', 'four', 'five', 'five', 'five', 'five', 'five', 'five'];
