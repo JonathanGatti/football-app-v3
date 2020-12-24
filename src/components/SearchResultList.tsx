09be0ad814951dd502b49da0e94d8050c336d665
jonathan gatti <jonathangatti2@gmail.com>
2020-12-24 15:53:04 +0000

    searchResultList styles

--- src/components/SearchResultList.tsx
+++ src/components/SearchResultList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Player } from '../interfaces';
 import { Button, Icon, List } from 'semantic-ui-react';
 
@@ -8,20 +9,35 @@ interface SearchResultListProps {
   onOpen: Function;
 }
 
+const ListDiv = styled.div`
+  display: flex;
+  width: 50vw;
+`;
 function SearchResultList({ player, onClick, onOpen }: SearchResultListProps) {
   return (
-    <List.Item>
-      <Icon name="user" />
-      {player.player_name}
-      <Button
-        onClick={() => {
-          onOpen(false);
-          onClick(player);
+    <ListDiv>
+      <List.Item
+        style={{
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'space-between',
         }}
       >
-        Add To The Team
-      </Button>
-    </List.Item>
+        <div>
+          <Icon name="user" />
+          {player.player_name}
+        </div>
+        <Button
+          size="small"
+          onClick={() => {
+            onOpen(false);
+            onClick(player);
+          }}
+        >
+          Add To The Team
+        </Button>
+      </List.Item>
+    </ListDiv>
   );
 }
 
