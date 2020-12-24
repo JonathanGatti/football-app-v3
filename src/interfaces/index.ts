2ea19a985633386d0166e218fa6e3f67a8ec9c93
jonathan gatti <jonathangatti2@gmail.com>
2020-12-24 10:27:04 +0000

    search player form and Player interface

---
+++ src/interfaces/index.ts
@@ -0,0 +1,15 @@
+export interface Player {
+  age: number;
+  birth_country: string;
+  birth_date: string;
+  birth_place: string;
+  firstname: string;
+  height: string;
+  lastname: string;
+  nationality: string;
+  number: null;
+  player_id: number;
+  player_name: string;
+  position: string;
+  weight: string;
+}
--- src/pages/CreateTeam.tsx
+++ src/pages/CreateTeam.tsx
@@ -1,26 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import { getPlayer } from '../api/externalApi';
+import { Player } from '../interfaces';
 
 function CreateTeam() {
   const [name, setName] = useState('');
-  // useEffect(() => {
-  //   async function getPlayerInEffect() {
-  //     const res = await getPlayer('');
-  //     console.log(res);
-  //   }
-  //   getPlayerInEffect();
-  // }, []);
+  const [searchResult, setSearchResult] = useState([]);
 
-  const handleSubmit = (e: any) => {
-    e.stopPropagation();
-    console.log(e.target.value);
+  const handleSubmit = async (e: any) => {
+    const res = await getPlayer(name);
+    console.log(res);
+    setSearchResult(res);
+  };
+
+  const handleChange = (e: any) => {
+    setName(e.target.value);
+    console.log(name);
   };
   return (
     <div>
       <h1>Create Team</h1>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <input type="text" />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit(e);
+        }}
+      >
+        <input type="text" onChange={handleChange} />
       </form>
+      <ul>
+        {searchResult.map((player: Player) => (
+          <li key={player.player_id}>{player.player_name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
